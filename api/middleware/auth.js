84f66ee1b932c5d42c3d9261e41f010bf3fb0b9a
fortsharif <huseinsharif.96@gmail.com>
2021-06-22 12:27:14 +0100

    authorization on applications

---
+++ api/middleware/auth.js
@@ -0,0 +1,12 @@
+jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+        const decoded = jwt.verify(token, process.env.JWT)
+        req.userData = decoded;
+        next()
+    } catch (e) {
+        return res.json({ error: `Auth failed` })
+    }
+}
--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const applications = require('./api/route/applications')
 const user = require('./api/route/user')
+const auth = require('./api/middleware/auth')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1/applications", applications)
+app.use("/api/v1/applications", auth, applications)
 app.use("/api/v1/user", user)
 app.use("*", (req, res) => res.status(404).json({ error: "404 Page not found" }))
 
